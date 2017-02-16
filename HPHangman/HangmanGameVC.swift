212e37f301bd57e940a8775ccdaaacb431813cc0
bmanjuu <bettyofung@gmail.com>
2017-02-16 09:03:24 -0500

    started working on view controller for hangman game

---
+++ HPHangman/HangmanGameVC.swift
@@ -0,0 +1,37 @@
+//
+//  HangmanGameVC.swift
+//  HPHangman
+//
+//  Created by Betty Fung on 2/16/17.
+//  Copyright © 2017 Betty Fung. All rights reserved.
+//
+
+import UIKit
+
+class HangmanGameVC: UIViewController {
+
+    @IBOutlet weak var hangmanImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
